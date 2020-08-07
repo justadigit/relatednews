fc7945c54978b6646fb55136db19222c4116e651
justadigit <letmecode123456@gmail.com>
2020-08-07 21:50:33 +0630

    Adding News to Related Categories

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -1,76 +1,96 @@
 // index , detail, post_categories , delete_categories , get_editcategory , update_category
 const Category = require('../../models/Category');
-const index = (req,res)=>{
-    Category.find()
-    .then(data=>{
-        if(data.length>0){
-            res.status(200).json(data);
-        }else{
-            res.status(200).json({"Message":"No Data Yet!"});
-        }
+const News = require('../../models/News');
+const { ensureIndexes } = require('../../models/Category');
+const index = (req, res) => {
+  Category.find()
+    .then((data) => {
+      if (data.length > 0) {
+        res.status(200).json(data);
+      } else {
+        res.status(200).json({ Message: 'No Data Yet!' });
+      }
     })
-    .catch(err=>{
-        res.status(500).json(err);
-    })
-}
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
 
-const detail = (req,res)=>{
-    const id = req.params.id;
-    Category.findById(id)
-    .then(data=>{
-        res.status(200).json(data)
-    })
-    .catch(err=>{
-        res.status(500).json(err);
-    })
-}
-
-const post_categories = (req,res)=>{
-    const name = req.body.name;
-    const category = new Category({
-        name:name
-    })
-    category.save()
-    .then(data=>{
-        res.status(200).json(data)
-    })
-    .catch(err=>{
-        res.status(500).json(err)
-    })
-}
-
-const delete_categories = (req,res)=>{
-    const id = req.params.id;
-    Category.findByIdAndDelete(id)
-    .then(data=>{
-        res.status(200).json({"Message":"Delete Successfully!"})
-    })
-    .catch(err=>{
-        res.status(500).json(err)
-    })
-}
-
-const update_categories = (req,res)=>{
-    const id = req.params.id;
-    Category.findByIdAndUpdate(id,{
-        name:req.body.name
-    },(err,doc)=>{
-        if(!err){
-            res.status(200).json({
-                message:"Update SuccessFully!",
-                data :doc
-            })
-        }else{
+const detail = (req, res, next) => {
+  const name = req.params.name;
+  Category.find({ name: name })
+    .then((getdata) => {
+      if (getdata.length > 0) {
+        News.find({ categoryId: getdata[0]._id })
+          .then((news) => {
+            if (news.length > 0) {
+              res.status(200).json(news);
+            } else {
+              res.status(404).json('No Data for This!');
+            }
+          })
+          .catch((err) => {
             res.status(500).json(err);
-        }
+          });
+      } else {
+        res.status(404).json('Not Found!');
+      }
     })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+};
 
-}
+const post_categories = (req, res) => {
+  const name = req.body.name;
+  const category = new Category({
+    name: name,
+  });
+  category
+    .save()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
 
-module.exports ={
-    index,
-    detail,
-    post_categories,
-    delete_categories,
-    update_categories
-}
+const delete_categories = (req, res) => {
+  const id = req.params.id;
+  Category.findByIdAndDelete(id)
+    .then((data) => {
+      res.status(200).json({ Message: 'Delete Successfully!' });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+const update_categories = (req, res) => {
+  const id = req.params.id;
+  Category.findByIdAndUpdate(
+    id,
+    {
+      name: req.body.name,
+    },
+    (err, doc) => {
+      if (!err) {
+        res.status(200).json({
+          message: 'Update SuccessFully!',
+          data: doc,
+        });
+      } else {
+        res.status(500).json(err);
+      }
+    }
+  );
+};
+
+module.exports = {
+  index,
+  detail,
+  post_categories,
+  delete_categories,
+  update_categories,
+};
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const categoriesController = require('../controllers/categories');
 
-router.get('/',categoriesController.index)
-router.get('/:id',categoriesController.detail);
-router.post('/',categoriesController.post_categories)
-router.delete('/:id',categoriesController.delete_categories)
-router.put('/:id',categoriesController.update_categories)
+router.get('/', categoriesController.index);
+router.get('/:name', categoriesController.detail);
+router.post('/', categoriesController.post_categories);
+router.delete('/:id', categoriesController.delete_categories);
+router.put('/:id', categoriesController.update_categories);
 
 module.exports = router;

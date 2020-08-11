a313a44545a33fd72660295dc03a770ea6379cd6
justadigit <letmecode123456@gmail.com>
2020-08-11 16:31:18 +0630

    Authentication with JWT

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const checkAuth = require('../../middleware/check-auth');
+const checkAuth = require('../../middleware/checkAuth');
 const categoriesController = require('../controllers/categories');
 
 router.get('/', categoriesController.index);
--- api/routes/news.js
+++ api/routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const checkAuth = require('../../middleware/check-auth');
+const checkAuth = require('../../middleware/checkAuth');
 
 const newsController = require('../controllers/news');
 
